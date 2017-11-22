4ae268d538912baca6e76a8f4500ebe44b30f474
Kruthika Vijay Somayajula <33083064+kruthika-vijay@users.noreply.github.com>
2017-11-22 17:21:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $email =  $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $formcontent="Submitted Information: \n \n Name: \n $name \n \n Email: \n $email \n \n  Message: \n $message";
-$recipient = "suresh.kori@avetoconsulting.com";
+$recipient = "yourmail@domain.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
