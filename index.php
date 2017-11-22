5bfdeaaab6b94fac9ded0084de9c81304dea3121
Kruthika Vijay Somayajula <33083064+kruthika-vijay@users.noreply.github.com>
2017-11-22 17:20:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php $name = $_POST['name'];
+echo $name;
+$email =  $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$formcontent="Submitted Information: \n \n Name: \n $name \n \n Email: \n $email \n \n  Message: \n $message";
+$recipient = "suresh.kori@avetoconsulting.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+header('Location: /index.html');
+exit;
+?>
